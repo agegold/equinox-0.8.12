c2ef81225c11d38d073570592663f540e688bf99
parksunkyu81 <parksunkyu81@gmail.com>
2021-12-31 12:20:32 +0900

    조향 튜닝

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -86,23 +86,21 @@ class CarInterface(CarInterfaceBase):
 
     # lateral
     ret.lateralTuning.init('lqr')
-
-    ret.lateralTuning.lqr.scale = 1550.
-    ret.lateralTuning.lqr.ki = 0.01
-    ret.lateralTuning.lqr.dcGain = 0.0027
-
+    ret.lateralTuning.lqr.scale = 1700.0
+    ret.lateralTuning.lqr.ki = 0.03
+    ret.lateralTuning.lqr.dcGain = 0.003
     ret.lateralTuning.lqr.a = [0., 1., -0.22619643, 1.21822268]
     ret.lateralTuning.lqr.b = [-1.92006585e-04, 3.95603032e-05]
     ret.lateralTuning.lqr.c = [1., 0.]
-    ret.lateralTuning.lqr.k = [-110., 451.]
-    ret.lateralTuning.lqr.l = [0.33, 0.318]
+    ret.lateralTuning.lqr.k = [-105.0, 450.0]
+    ret.lateralTuning.lqr.l = [0.22, 0.318]
 
     ret.steerRatio = 16.5
     ret.steerActuatorDelay = 0.1
     ret.steerLimitTimer = 2.5
-    ret.steerRateCost = 0.35
+    ret.steerRateCost = 0.4
     ret.steerMaxBP = [0.]
-    ret.steerMaxV = [2.]
+    ret.steerMaxV = [1.5]
 
     # TODO: get actual value, for now starting with reasonable value for
     # civic and scaling by mass and wheelbase
