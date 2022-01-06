18f7db43e647c90b44a4d8694f5b6b7728c48a4b
parksunkyu81 <parksunkyu81@gmail.com>
2022-01-07 02:25:33 +0900

    속도튜닝

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -85,14 +85,14 @@ class CarInterface(CarInterfaceBase):
 
     # lateral
     ret.lateralTuning.init('lqr')
-    ret.lateralTuning.lqr.scale = 1975.0
-    ret.lateralTuning.lqr.ki = 0.032
+    ret.lateralTuning.lqr.scale = 1700.0
+    ret.lateralTuning.lqr.ki = 0.03
+    ret.lateralTuning.lqr.dcGain = 0.003
     ret.lateralTuning.lqr.a = [0., 1., -0.22619643, 1.21822268]
     ret.lateralTuning.lqr.b = [-1.92006585e-04, 3.95603032e-05]
     ret.lateralTuning.lqr.c = [1., 0.]
-    ret.lateralTuning.lqr.k = [-110.73572306, 451.22718255]
-    ret.lateralTuning.lqr.l = [0.3233671, 0.3185757]
-    ret.lateralTuning.lqr.dcGain = 0.002237852961363602
+    ret.lateralTuning.lqr.k = [-105.0, 450.0]
+    ret.lateralTuning.lqr.l = [0.22, 0.318]
 
     ret.steerRatio = 16.8
     ret.steerActuatorDelay = 0.0
@@ -122,7 +122,7 @@ class CarInterface(CarInterfaceBase):
 
     # Neokii
     ret.longitudinalTuning.kpBP = [0., 10. * CV.KPH_TO_MS, 20. * CV.KPH_TO_MS, 130. * CV.KPH_TO_MS]
-    ret.longitudinalTuning.kpV = [1.3, 0.94, 0.8, 0.48]
+    ret.longitudinalTuning.kpV = [1.28, 0.94, 0.8, 0.48]
     ret.longitudinalTuning.kiBP = [0., 130. * CV.KPH_TO_MS]
     ret.longitudinalTuning.kiV = [0.18, 0.12]
     ret.longitudinalActuatorDelayLowerBound = 0.15
