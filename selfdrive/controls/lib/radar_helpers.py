b3f2e017678752818dd199331111c546c9309be1
parksunkyu81 <parksunkyu81@gmail.com>
2022-01-09 09:37:39 +0900

    튜닝

--- selfdrive/controls/lib/radar_helpers.py
+++ selfdrive/controls/lib/radar_helpers.py
@@ -5,7 +5,7 @@ from selfdrive.config import RADAR_TO_CAMERA
 
 # the longer lead decels, the more likely it will keep decelerating
 # TODO is this a good default?
-_LEAD_ACCEL_TAU = 5
+_LEAD_ACCEL_TAU = 7.5
 
 # radar tracks
 SPEED, ACCEL = 0, 1   # Kalman filter states enum
