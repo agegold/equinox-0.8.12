b4b5aaa1aed8e184a53404b3c8cb1dfb3380e7ba
parksunkyu81 <parksunkyu81@gmail.com>
2022-01-11 18:56:01 +0900

    브래이크, AEB 추가

--- selfdrive/car/gm/gmcan.py
+++ selfdrive/car/gm/gmcan.py
@@ -35,6 +35,9 @@ def create_gas_regen_command(packer, bus, throttle, idx, acc_engaged, at_full_st
   return packer.make_can_msg("ASCMGasRegenCmd", bus, values)
 
 def create_friction_brake_command(packer, bus, apply_brake, idx, near_stop, at_full_stop):
+  print("apply_brake : " + apply_brake)
+  print("near_stop : " + near_stop)
+  print("at_full_stop : " + at_full_stop)
   mode = 0x1
   if apply_brake > 0:
     mode = 0xa
