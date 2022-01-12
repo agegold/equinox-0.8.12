4378336cfa72917ba2b7b096fec52f824155cbb6
parksunkyu81 <parksunkyu81@gmail.com>
2022-01-12 11:51:22 +0900

    브래이크 비활성

--- selfdrive/car/gm/carcontroller.py
+++ selfdrive/car/gm/carcontroller.py
@@ -44,13 +44,13 @@ class CarController():
     self.steer_rate_limited = False
     
     self.accel_steady = 0.
-    self.apply_brake = 0
+    #self.apply_brake = 0
     
     self.params = CarControllerParams()
 
     self.packer_pt = CANPacker(DBC[CP.carFingerprint]['pt'])
-    self.packer_obj = CANPacker(DBC[CP.carFingerprint]['radar'])
-    self.packer_ch = CANPacker(DBC[CP.carFingerprint]['chassis'])
+    #self.packer_obj = CANPacker(DBC[CP.carFingerprint]['radar'])
+    #self.packer_ch = CANPacker(DBC[CP.carFingerprint]['chassis'])
 
   def update(self, enabled, CS, frame, actuators,
              hud_v_cruise, hud_show_lanes, hud_show_car, hud_alert):
@@ -94,7 +94,7 @@ class CarController():
 
       if not enabled or not CS.adaptive_Cruise or CS.out.vEgo <= 1 / CV.MS_TO_KPH:
         comma_pedal = 0.
-        apply_brake = 0
+        #apply_brake = 0
       elif CS.adaptive_Cruise and CS.out.vEgo > 1 / CV.MS_TO_KPH:
         # 이것이 없으면 저속에서 너무 공격적입니다.
         gas_mult = interp(CS.out.vEgo, [0., 10.], [0.4, 1.0])
@@ -103,17 +103,17 @@ class CarController():
         # OP 가 활성화되지 않았을 때 0이 아닌 가스를 보내면 PCM이 예상대로 스로틀에 응답하지 않습니다.
         # 활성화할 때.
         comma_pedal = clip(gas_mult * (gas - brake), 0., 1.)
-        apply_brake = int(round(interp(actuators.accel, P.BRAKE_LOOKUP_BP, P.BRAKE_LOOKUP_V)))
+        #apply_brake = int(round(interp(actuators.accel, P.BRAKE_LOOKUP_BP, P.BRAKE_LOOKUP_V)))
 
       if (frame % 4) == 0:
         idx = (frame // 4) % 4
 
-        at_full_stop = enabled and CS.out.standstill
-        near_stop = enabled and (CS.out.vEgo < P.NEAR_STOP_BRAKE_PHASE)
-        print("apply_brake : " , apply_brake)
-        print("near_stop : " , near_stop)
-        print("at_full_stop : ", + at_full_stop)
-        can_sends.append(gmcan.create_friction_brake_command(self.packer_ch, CanBus.CHASSIS, apply_brake, idx, near_stop,at_full_stop))
+        #at_full_stop = enabled and CS.out.standstill
+        #near_stop = enabled and (CS.out.vEgo < P.NEAR_STOP_BRAKE_PHASE)
+        #print("apply_brake : " , apply_brake)
+        #print("near_stop : " , near_stop)
+        #print("at_full_stop : ", + at_full_stop)
+        #can_sends.append(gmcan.create_friction_brake_command(self.packer_ch, CanBus.CHASSIS, apply_brake, idx, near_stop,at_full_stop))
         can_sends.append(create_gas_interceptor_command(self.packer_pt, comma_pedal, idx))
 
     # Show green icon when LKA torque is applied, and
