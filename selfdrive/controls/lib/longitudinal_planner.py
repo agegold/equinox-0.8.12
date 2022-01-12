6c954e7484a2338fa6c0008d7f9dd1127f619e68
parksunkyu81 <parksunkyu81@gmail.com>
2022-01-12 20:03:50 +0900

    Revert "LAT 활성화"
    This reverts commit 10300767

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -29,15 +29,15 @@ DP_ACCEL_NORMAL = 2
 DP_ACCEL_SPORT = 3
 
 # accel profile by @arne182 modified by @wer5lcy
-_DP_CRUISE_MIN_V_ECO = [-2.0, -1.6, -1.4, -1.2, -1.0]
-_DP_CRUISE_MIN_V_NORMAL = [-2.0, -1.8, -1.6, -1.4, -1.2]
-_DP_CRUISE_MIN_V_SPORT = [-3.0, -2.0, -1.8, -1.6, -1.4]
-_DP_CRUISE_MIN_BP = [0.0, 5.0, 10.0, 20.0, 30.0]
-
-#_DP_CRUISE_MIN_V_ECO = [-6.0, -6.0, -4.0, -3.5, -3.0]
-#_DP_CRUISE_MIN_V_NORMAL = [-6.5, -6.5, -5.0, -5.0, -4.0]
-#_DP_CRUISE_MIN_V_SPORT = [-7.0, -7.0, -6.0, -5.0, -4.5]
+#_DP_CRUISE_MIN_V_ECO = [-2.0, -1.6, -1.4, -1.2, -1.0]
+#_DP_CRUISE_MIN_V_NORMAL = [-2.0, -1.8, -1.6, -1.4, -1.2]
+#_DP_CRUISE_MIN_V_SPORT = [-3.0, -2.0, -1.8, -1.6, -1.4]
 #_DP_CRUISE_MIN_BP = [0.0, 5.0, 10.0, 20.0, 30.0]
+
+_DP_CRUISE_MIN_V_ECO = [-6.0, -6.0, -4.0, -3.5, -3.0]
+_DP_CRUISE_MIN_V_NORMAL = [-6.5, -6.5, -5.0, -5.0, -4.0]
+_DP_CRUISE_MIN_V_SPORT = [-7.0, -7.0, -6.0, -5.0, -4.5]
+_DP_CRUISE_MIN_BP = [0.0, 5.0, 10.0, 20.0, 30.0]
 
 _DP_CRUISE_MAX_V_ECO = [1.5, 1.3, 0.8, 0.6, 0.4]
 _DP_CRUISE_MAX_V_NORMAL = [1.6, 1.4, 1.0, 0.8, 0.6]
