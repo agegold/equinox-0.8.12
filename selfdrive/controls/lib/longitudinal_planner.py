a5c3cf049197ea158f90fa159df90b95f401dc97
parksunkyu81 <parksunkyu81@gmail.com>
2022-01-13 10:21:56 +0900

    앞차 거리 튜닝

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -29,20 +29,20 @@ DP_ACCEL_NORMAL = 2
 DP_ACCEL_SPORT = 3
 
 # accel profile by @arne182 modified by @wer5lcy
-_DP_CRUISE_MIN_V_ECO = [-2.0, -1.6, -1.4, -1.2, -1.0]
-_DP_CRUISE_MIN_V_NORMAL = [-2.3, -1.8, -1.6, -1.4, -1.2]
-_DP_CRUISE_MIN_V_SPORT = [-3.0, -2.6, -2.3, -2.0, -1.0]
-_DP_CRUISE_MIN_BP = [0.0, 5.0, 10.0, 20.0, 30.0]
+_DP_CRUISE_MIN_V_ECO = [-2.0, -1.6, -1.5, -1.4, -1.2, -1.0]
+_DP_CRUISE_MIN_V_NORMAL = [-2.3, -1.8, -1.7, -1.6, -1.4, -1.2]
+_DP_CRUISE_MIN_V_SPORT = [-3.0, -2.6, -2.4, -2.3, -2.0, -1.0]
+_DP_CRUISE_MIN_BP = [0.0, 5.0, 10.0, 15.0, 20.0, 30.0]
 
 #_DP_CRUISE_MIN_V_ECO = [-6.0, -6.0, -4.0, -3.5, -3.0]
 #_DP_CRUISE_MIN_V_NORMAL = [-6.5, -6.5, -5.0, -5.0, -4.0]
 #_DP_CRUISE_MIN_V_SPORT = [-7.0, -7.0, -6.0, -5.0, -4.5]
 #_DP_CRUISE_MIN_BP = [0.0, 5.0, 10.0, 20.0, 30.0]
 
-_DP_CRUISE_MAX_V_ECO = [1.4, 1.3, 1.0, 0.8, 0.4]
-_DP_CRUISE_MAX_V_NORMAL = [1.5, 1.4, 1.1, 1.0, 0.6]
-_DP_CRUISE_MAX_V_SPORT = [1.6, 1.5, 1.3, 1.1, 0.8]
-_DP_CRUISE_MAX_BP = [0., 5., 10., 20., 30.]
+_DP_CRUISE_MAX_V_ECO = [1.4, 1.3, 1.0, 0.9, 0.8, 0.4]
+_DP_CRUISE_MAX_V_NORMAL = [1.5, 1.4, 1.2, 1.1, 1.0, 0.6]
+_DP_CRUISE_MAX_V_SPORT = [1.6, 1.5, 1.4, 1.3, 1.1, 0.8]
+_DP_CRUISE_MAX_BP = [0., 5., 10., 15., 20., 30.]
 
 def dp_calc_cruise_accel_limits(v_ego, dp_profile):
   if dp_profile == DP_ACCEL_ECO:
